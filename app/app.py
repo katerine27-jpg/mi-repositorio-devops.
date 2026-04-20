980dd4f8b9b174b26e8e36aa509d5fab14f4455a
student <al07094967@tecmilenio.mx>
2026-04-20 06:54:20 -0700

    fix app.py

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Pipeline actualizado automáticamente 🚀""
+    return "Pipeline funcionando correctamente"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
